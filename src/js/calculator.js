4e7535daf4d097a32b9127a99185187cf41db58c
Alec Burch-DeWitt <168141387+alecthedev@users.noreply.github.com>
2025-09-02 18:42:36 +0000

    add quinaryToDecimal and decimalToQuinary functions

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -14,9 +14,27 @@ function divide(a, b) {
     return a / b;
 }
 
+function quinaryToDecimal(quinary) {
+    return parseInt(quinary, 5);
+}
+
+function decimalToQuinary(decimal) {
+    if (decimal === 0) return "0";
+    let quinary = "";
+    let quotient = decimal;
+    while (quotient > 0) {
+        quinary = (quotient % 5).toString() + quinary;
+        quotient = Math.floor(quotient / 5);
+  }
+  return quinary;
+}
+
+
 module.exports = {
     add,
     subtract,
     multiply,
-    divide
+    divide,
+    quinaryToDecimal,
+    decimalToQuinary
 }
