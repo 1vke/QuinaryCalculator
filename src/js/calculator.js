29cbcca5dc85da7a7ad2316a517060bb2f453831
Benjamin Britton <120000566+bennyjoseph23@users.noreply.github.com>
2025-09-02 18:06:22 -0400

    Revert "Conversion logic + tests"

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -14,27 +14,9 @@ function divide(a, b) {
     return a / b;
 }
 
-function quinaryToDecimal(quinary) {
-    return parseInt(quinary, 5);
-}
-
-function decimalToQuinary(decimal) {
-    if (decimal === 0) return "0";
-    let quinary = "";
-    let quotient = decimal;
-    while (quotient > 0) {
-        quinary = (quotient % 5).toString() + quinary;
-        quotient = Math.floor(quotient / 5);
-  }
-  return quinary;
-}
-
-
 module.exports = {
     add,
     subtract,
     multiply,
-    divide,
-    quinaryToDecimal,
-    decimalToQuinary
+    divide
 }
